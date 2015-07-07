3a2210fdc81608f3db7be1d89a663a3474169949
Peter Wong <peterpaulwong@Peter-Wongs-MacBook-Pro.local>
2015-07-06 23:52:13 -0500

    deleting old files

--- django/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/v/filer4b/v38q001/peterp/CSN373/8ByteDining/cs373-idb/django/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
