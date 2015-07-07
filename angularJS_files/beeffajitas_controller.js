e2d52e9305279ca149010f44f28b97bf323a02d0
Mario <mario771@cs.utexas.edu>
2015-07-06 19:00:54 -0500

    updated JS controllers with nutritional info

--- angularJS_files/beeffajitas_controller.js
+++ angularJS_files/beeffajitas_controller.js
@@ -2,7 +2,7 @@ app.controller('RecipeController', function($scope) {
 	$scope.test = "working!"
 	$scope.BeefFajitas = {
 		name: "Beef Fajitas",
-		//not sure if this works for the img...looking into it
+		cuisine: "Mexican Cuisine"
 		img: "https://api.pearson.com/kitchen-manager/v1/images/full/beef_fajitas.jpg",
 		serving: "Serves 6, Yields 6",
 		ingredients: [
@@ -22,9 +22,22 @@ app.controller('RecipeController', function($scope) {
 			"6 sprigs fresh cilantro",
 		],
 		directions: "Make the marinade by chopping and mashing the garlic into a paste. In a bowl, combine the garlic paste with the remaining marinade ingredients.Trim the fat from the skirt steak. Cut the steak into two or three pieces if necessary. Add the steaks to the marinade, turning them several times to coat all sides. Cover the steak and mari-nate in the refrigerator for at least 1 hour or overnight. Grill the steak on a hot grill to the desired doneness. Remove the steak and allow it to rest for 10 minutes. Add the oil to a heavy sautee pan, heat the pan until very hot and sautee the bell peppers, onion and garlic just until they begin to soften. Slice the steak against the grain into thin slices. Arrange the steak and the pepper mixture on very hot cast-iron platters and garnish with the cilantro. The platters should be sizzling as they are presented to the table. Serve the fajitas accompanied by warm flour or corn tortillas, fresh salsa, sour cream, and guacamole.",
-		nutritional_info: [
+		nutritional_info: {
+			"Calcium" :  "91.79 mg per 100g",
+			"Calories" : "399.79 kcal per 100g",
+			"Calories_fat" : "155.8 g per 100g",
+			"Carbohydrates" : "16.28 g per 100g",
+			"Cholesterol" : "68.04 mg per 100g",
+			"Fat" : "17.31 g per 100g",
+			"Iron" : "4.46 mg per 100g",
+			"Protein" :	"44.88 g per 100g",
+			"Saturated_fat" : "8.63 g per 100g",
+			"Sodium" :	"706.47 mg per 100g",
+			"Trans_fat" : "-1 g per 100g",
+			"Vitamin_A" : "2183.87 IU per 100g",
+			"Vitamin_C" : "125.93 mg per 100g", 
 
-		]
+		}
 	}
 	$scope.recipeblurb = "Mexican Cuisine # will link to all Mexican cuisine recipes (grid of linkable pictures with recipe names)"
 
--- angularJS_files/biscotti_controller.js
+++ angularJS_files/biscotti_controller.js
@@ -2,7 +2,7 @@ app.controller('RecipeController', function($scope) {
 	$scope.test = "working!"
 	$scope.Biscotti = {
 		name: "Biscotti",
-		//not sure if this works for the img...looking into it
+		cuisine: "Italian Cuisine",
 		img: "https://api.pearson.com/kitchen-manager/v1/images/full/biscotti.jpg",
 		serving: "Serves 36, Yields 36",
 		ingredients: [
@@ -18,9 +18,22 @@ app.controller('RecipeController', function($scope) {
     		"semisweet chocolate (as needed)",
 		],
 		directions: "Sift together the cinnamon and ammonium carbonate or baking powder. Stir in the hazelnut, almond and pastry flours. Set aside. In a large bowl, whisk together the eggs and sugar to the ribbon stage, approximately 3 minutes. Add the butter. Stir in the flour mixture with a rubber spatula, then stir in the whole hazelnuts. Divide the dough into three even pieces. Refrigerate until cold. Roll each piece of dough into a 12-inch (30-centimeter) log. Place on a paper-lined sheet pan, leaving at least 3 inches (7.5 centimeters) of space between each log. Bake at 350F (180C) until golden in color, approximately 20 minutes. Cool the logs, then slice them into 1-inch- (3-centimeter-) thick slices. Place the sliced cookies upright on paper-lined sheet pans. Double-tray the pans. Reduce heat to 325F (160C) and bake until the biscotti are thoroughly crisp, approximately 40 minutes. Once cool, the biscotti may be dipped in tempered chocolate.",
-		nutritional_info: [
+		nutritional_info: {
+			"Calcium" :  "48.37 mg per 100g",
+			"Calories" : "245.62 kcal per 100g",
+			"Calories_fat" : "139.98 g per 100g",
+			"Carbohydrates" : "25.08 g per 100g",
+			"Cholesterol" : "40.19 mg per 100g",
+			"Fat" : "15.55 g per 100g",
+			"Iron" : "1.37 mg per 100g",
+			"Protein" :	"4.53 g per 100g",
+			"Saturated_fat" : "4.19 g per 100g",
+			"Sodium" :	"27.78 mg per 100g",
+			"Trans_fat" : "0 g per 100g",
+			"Vitamin_A" : "194.56 IU per 100g",
+			"Vitamin_C" : "0.66 mg per 100g",
 			
-		]
+		}
 	}
 	$scope.recipeblurb = "Italian Cuisine # will link to all Italian cuisine recipes (grid of linkable pictures with recipe names)"
 
