6344c6471e90189385919535ad777813a8165295
Peter Wong <peterpaulwong@Peter-Wongs-MacBook-Pro.local>
2015-07-06 23:30:22 -0500

    adding django models changing

---
+++ django/src/food/models.py
@@ -0,0 +1,73 @@
+from django.db import models
+
+# Create your models here.
+
+#-------------
+#Recipe Model
+#-------------
+
+class Recipes(models.Model) : 
+
+ """ 
+  The Recipe model returns name, id (recipe name)  url,
+  cusine, cooking method, ingredients, image, and thumb
+ """
+
+ name = models.CharField(max_length=500)
+ recipe_id = models.CharField(max_length=500)
+ url = models.CharField(max_length=500)
+ cusine = models.CharField(max_length=500)
+ cooking_method = models.CharField(max_length = 500)
+ # How to put ingredients into list?? I think there's models.ManytoMany ?
+ image = models.CharField(max_length=500) 
+ thumb = models.CharField(max_length =500)
+
+ def get_absolute_url(self):
+        url_name = self.full_name.replace(' ', '_')
+        return "/Recipes/%s/" % url_name
+
+ def __str__ (self):
+        return self.full_name
+  
+#------------------
+#Ingredients Model
+#------------------
+
+class Ingredients(models.Model) :
+
+   """
+   
+   """
+   
+  
+   def get_absolute_url(self):
+        url_name = self.full_name.replace(' ', '_')
+        return "/ingredients/%s/" % url_name
+
+   def __str__ (self):
+        return self.full_name
+
+#------------
+#Cusine Model
+#------------
+
+class Cusine (models.Model) :
+
+   """
+   Cusine contains a name, an id (name), url, and recipe count
+   """
+
+   name = models.CharField(max_length=500)
+   cusine_id = models.CharField(max_length=500)
+   url = models.CharField(max_length=500)
+   recipe_count = models.IntegerField(default=0)
+
+   def get_absolute_url(self):
+        url_name = self.full_name.replace(' ', '_')
+        return "/cusine/%s/" % url_name
+
+   def __str__ (self):
+        return self.full_name
+
+
+
