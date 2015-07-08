c36293059fcb02535c02f15e62fa39828f1e5540
Peter Wong <peterpaulwong@wireless-10-147-38-110.public.utexas.edu>
2015-07-08 16:10:53 -0500

    adding updated urls config

--- food/foodApp/views.py
+++ food/foodApp/views.py
@@ -3,9 +3,18 @@ from django.shortcuts import RequestContext
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
 
-def index(request) :
-   return HttpResponse("Hello, world. You're at the food index.")
-
-def potroast (request) :
+def home(request):
    context = RequestContext(request)
-   return render_to_response('auntruthiespotroast.html',context)
+   return render_to_response('8byte_splash.html',context)
+
+def Cuisines (request) :
+   context = RequestContext(request)
+   return render_to_response('Cuisines.html',context)
+
+def Ingredients (request) :
+   context = RequestContext(request)
+   return render_to_response('Ingredients.html',context)
+
+def Recipes (request) :
+   context = RequestContext(request)
+   return render_to_response('Recipes.html',context)
