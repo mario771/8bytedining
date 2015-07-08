d00031c884e187df0813dfdffcafa6ca08c916ba
Derek Cornelius <drc@cs.utexas.edu>
2015-07-07 20:34:13 -0500

    prepping for model page creation

--- static_angular_html_files/js/main.js
+++ static_angular_html_files/js/main.js
@@ -5,22 +5,26 @@ app.controller('RecipeController', function($scope, $http) {
 	$http.get('data/data.json').then(function(res) {
 		$scope.recipies = res.data;
 	});
-/*
-	$http.get('data/recipemodels.json').then(function(res) {
+	$http.get('data/recipemodels_placeholder.json').then(function(res) {
 		$scope.rmodels = res.data;
 	});
-*/
 });
 
 app.controller('CuisineController', function($scope, $http) {
 	$http.get('data/cuisines_.json').then(function(res) {
 		$scope.cuisines = res.data;
 	});
+	$http.get('data/recipemodels_placeholder.json').then(function(res) {
+		$scope.cmodels = res.data;
+	});
 });
 
 app.controller('IngredientController', function($scope, $http) {
 	$http.get('data/Ingredients_.json').then(function(res) {
 		$scope.ingredients = res.data;
+	});
+	$http.get('data/recipemodels_placeholder.json').then(function(res) {
+		$scope.imodels = res.data;
 	});
 });
 
