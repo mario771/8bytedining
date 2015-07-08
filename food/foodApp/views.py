0e02af25231dee2723faece52b252f2fa54679d4
Peter Wong <peterpaulwong@wireless-10-147-38-110.public.utexas.edu>
2015-07-07 21:13:23 -0500

    adding django config for testing purposes

--- food/foodApp/views.py
+++ food/foodApp/views.py
@@ -1,7 +1,11 @@
-#from django.shortcuts import render
-
+from django.shortcuts import render
+from django.shortcuts import RequestContext
+from django.shortcuts import render_to_response
 from django.http import HttpResponse
 
 def index(request) :
    return HttpResponse("Hello, world. You're at the food index.")
 
+def potroast (request) :
+   context = RequestContext(request)
+   return render_to_response('auntruthiespotroast.html',context)
