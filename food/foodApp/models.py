9f786cdd21edb7cdcedc0895f10d9f9c13a3c2c0
JesusDeLaO <delao27>
2015-07-07 19:35:14 -0500

    pushing models.py

--- food/foodApp/models.py
+++ food/foodApp/models.py
@@ -10,17 +10,19 @@ class Recipes (models.Model) :
 
  """ 
   The Recipe model returns name, id (recipe name)  url,
-  cuisine, cooking method, ingredients, image, and thumb
+  cuisine, cooking method, ingredients, and image
  """
 
  name = models.CharField(max_length=500)
  recipe_id = models.CharField(max_length=500)
  url = models.CharField(max_length=500)
- cuisine = models.ForeignKey(Cuisine)
- cooking_method = models.CharField(max_length = 500)
+ directions = models.CharField(max_length= 5000, default = "")
+ nutritionalInfo = models.CharField(max_length=5000, default = "")
+ cuisineGivenRecipe = models.CharField(max_length=500)
+ ingredients = models.ForeignKey("Ingredients", default = "")
+ 
  # How to put ingredients into list?? I think there's models.ManytoMany ?
- image = models.CharField(max_length=500) 
- thumb = models.CharField(max_length=500)
+ image = models.CharField(max_length=500)
 
  def get_absolute_url(self):
         url_name = self.full_name.replace(' ', '_')
@@ -35,9 +37,7 @@ class Recipes (models.Model) :
 
 class Ingredients (models.Model) :
 
-   """
-   
-   """
+   name = models.CharField(max_length=500, default="")
    
   
    def get_absolute_url(self):
@@ -51,7 +51,7 @@ class Ingredients (models.Model) :
 #Cuisine Model
 #------------
 
-class Cuisine (models.Model) :
+class Cuisine(models.Model) :
 
    """
    Cuisine contains a name, an id (name), url, and recipe count
@@ -61,7 +61,8 @@ class Cuisine (models.Model) :
    cuisine_id = models.CharField(max_length=500)
    url = models.CharField(max_length=500)
    recipe_count = models.IntegerField(default=0)
-
+   ingredients = models.ForeignKey('Ingredients', default = "")
+   recipes = models.ForeignKey("Recipes", default = "")
    def get_absolute_url(self):
         url_name = self.full_name.replace(' ', '_')
         return "/cuisine/%s/" % url_name
--- food/foodApp/tests.py
+++ food/foodApp/tests.py
@@ -1,3 +1,55 @@
-from django.test import TestCase
+#Test for models.py
+import os
+import sys
+import json
+from django.test.utils import setup_test_environment
+from django.core.urlresolvers import reverse
+from django.core.management import call_command
 
+from django.utils import unittest
+from django.test import TestCase
+from django.http import HttpResponse
+
+from json import dumps, loads
+
+from models.py import *
+
+class ModelTestCase(TestCase):
+
+	#---------------------
+	# Recipe Model
+	#---------------------
+
+	def test_recipes_model1(self):
+            Recipes.objects.create(name= 'Irish Soda Bread', recipe_id = 'Irish Soda Bread', url ="https://api.pearson.com/kitchen-manager/v1/recipes/irish-soda-bread", directions = 'Soak the currants in the whiskey until plump, at least 1 hour. Sift the dry ingredients together. Stir in the currants and whiskey. Stir in the buttermilk, making a stiff batter. Spread the batter in a greased 8-inch (20-centimeter) round cake pan. Bake at 350Â°F (180Â°C) until well browned and firm, approximately 30 minutes.', nutritionalInfo =""  , cuisineGivenRecipe = "British Isles", ingredients ="salt"  )
+            recipe_get = Recipes.objects.get(name= "Irish Soda Bread")
+            self.assertEqual(recipe_get.name, "Irish Soda Bread")
+            self.assertEqual(recipe_get.url, "https://api.pearson.com/kitchen-manager/v1/recipes/irish-soda-bread")
+            self.assertEqual(recipe_get.directions, 'Soak the currants in the whiskey until plump, at least 1 hour. Sift the dry ingredients together. Stir in the currants and whiskey. Stir in the buttermilk, making a stiff batter. Spread the batter in a greased 8-inch (20-centimeter) round cake pan. Bake at 350Â°F (180Â°C) until well browned and firm, approximately 30 minutes.')
+            self.assertEqual(recipe_get.nutritionalInfo, "")
+            self.assertEqual(recipe_get.cuisineGivenRecipe, "British Isles")
+            self.assertEqual(recipe_get.ingredients, "salt")
+
+        def test_recipes_model2(self):
+            Recipes.objects.create(name= "Almond Cream", recipe_id = "Almond Cream", url ="https://api.pearson.com/kitchen-manager/v1/recipes/almond-cream", directions = "Cream the butter and sugar. Slowly add the eggs, scraping down the bowl as necessary. Stir the flour and almonds together, then add to the butter mixture. Blend until no lumps re-main. Almond cream may be stored under refrigeration for up to 3 weeks. ", nutritionalInfo =""  , cuisineGivenRecipe = "French", ingredients ="eggs"  )
+            recipe_get = Recipes.objects.get(name= "Almond Cream")
+            self.assertEqual(recipe_get.name, "Almond Cream")
+            self.assertEqual(recipe_get.url, "https://api.pearson.com/kitchen-manager/v1/recipes/almond-cream")
+            self.assertEqual(recipe_get.directions, "Cream the butter and sugar. Slowly add the eggs, scraping down the bowl as necessary. Stir the flour and almonds together, then add to the butter mixture. Blend until no lumps re-main. Almond cream may be stored under refrigeration for up to 3 weeks. ")
+            self.assertEqual(recipe_get.nutritionalInfo, "")
+            self.assertEqual(recipe_get.cuisineGivenRecipe, "French")
+            self.assertEqual(recipe_get.ingredients, "eggs")
+
+        def test_recipes_model3(self):
+            Recipes.objects.create(name= 'Irish Soda Bread', recipe_id = 'Irish Soda Bread', url ="https://api.pearson.com/kitchen-manager/v1/recipes/irish-soda-bread", directions = 'Soak the currants in the whiskey until plump, at least 1 hour. Sift the dry ingredients together. Stir in the currants and whiskey. Stir in the buttermilk, making a stiff batter. Spread the batter in a greased 8-inch (20-centimeter) round cake pan. Bake at 350Â°F (180Â°C) until well browned and firm, approximately 30 minutes.', nutritionalInfo =""  , cuisineGivenRecipe = "British Isles", ingredients ="salt"  )
+            recipe_get = Recipes.objects.get(name= "Irish Soda Bread")
+            self.assertEqual(recipe_get.name, "Irish Soda Bread")
+            self.assertEqual(recipe_get.url, "https://api.pearson.com/kitchen-manager/v1/recipes/irish-soda-bread")
+            self.assertEqual(recipe_get.directions, 'Soak the currants in the whiskey until plump, at least 1 hour. Sift the dry ingredients together. Stir in the currants and whiskey. Stir in the buttermilk, making a stiff batter. Spread the batter in a greased 8-inch (20-centimeter) round cake pan. Bake at 350Â°F (180Â°C) until well browned and firm, approximately 30 minutes.')
+            self.assertEqual(recipe_get.nutritionalInfo, "")
+            self.assertEqual(recipe_get.cuisineGivenRecipe, "British Isles")
+            self.assertEqual(recipe_get.ingredients, "salt")
+            
+            
+            
 # Create your tests here.
