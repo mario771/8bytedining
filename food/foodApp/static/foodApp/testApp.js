515778f70af7cf008f1b4ba0493c9571e236c23a
Derek Cornelius <drc@cs.utexas.edu>
2015-07-13 16:04:27 -0500

    testing angularjs location url function in index1

--- food/foodApp/static/foodApp/testApp.js
+++ food/foodApp/static/foodApp/testApp.js
@@ -11,27 +11,16 @@ app.controller("TestController", function($scope) {
 
 });
 
-/*
 app.controller("LocationController", function($scope, $location, $http) {
 	$scope.$location = {};
 	angular.forEach("protocol host port path search hash".split(" "), function(method){
 		$scope.$location[method] = function(){
 			var result = $location[method].call($location);
-			return angular.toJson(result);
-			//return angular.isObject(result) ? angular.toJson(result) : result;
+			return angular.isObject(result) ? angular.toJson(result) : result;
 		};
-	});
-
-	$http.get('data/info.json').then(function(res) {
-		$scope.blah = res.data;
 	});
 
 	$scope.url = $location.url();
 
 	$scope.test = "working";
-	
-	//$location.url().then(function(res2) {
-	//	$scope.test = res2;
-	//});
 });
-*/
