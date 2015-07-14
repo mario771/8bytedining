4454fd2213e144f03f210eca6262d88ad1b9cf80
Derek Cornelius <drc@cs.utexas.edu>
2015-07-14 12:33:10 -0500

    fixing dynamic recipe page edit

--- food/food/urls.py
+++ food/food/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.home,name='home'),
     url(r'^About$', views.About, name='About'),
-
+"""
     url(r'^IngredientModel$', views.IngredientsModel, name='IngredientsModel'),
     url(r'^RecipeModel$', views.RecipesModel, name='RecipesModel'),
     url(r'^CuisineModel$', views.CuisinesModel, name='CuisinesModel'),
@@ -23,7 +23,7 @@ urlpatterns = [
     url(r'^Recipes$', views.Recipes, name='Recipes'),
     url(r'^American$', views.American, name='American'),
     url(r'^Italian$', views.Italian, name='Italian'),
-
+"""
     url(r'^Index1$', views.Index1, name='Index1'),
     url(r'^Index2$', views.Index2, name='Index2'),
     url(r'^recipe_page$', views.recipe_page, name='recipe_page'),
