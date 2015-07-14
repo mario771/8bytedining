cfa5c68fb535bf9cd601a773e19a2157567fb636
Peter Wong <peterpaulwong@wireless-10-147-38-110.public.utexas.edu>
2015-07-14 12:51:45 -0500

    editing api settings

--- food/foodApp/serializers.py
+++ food/foodApp/serializers.py
@@ -4,17 +4,40 @@ from models import Recipes, Ingredients, Cuisines
 class RecipesSerializer(serializers.ModelSerializer) : 
     class Meta:
         model = Recipes 
-        field = (id,name,recipe_id,directions, cuisine, ingredients,img,nut_info,quant_dat)
+        fields = (
+           'id',
+           'name',
+           'recipe_id',
+           'directions',
+           'cuisine',
+           'ingredients',
+           'img',
+           'nut_info',
+           'quant_dat'
+            )
 
 class IngredientsSerializer(serializers.ModelSerializer) : 
     class Meta: 
         model = Ingredients
-        field = (id,ing_id,name,recipe_count,quant_data,nut_info)
+        fields = (
+            'id',
+            'ing_id', 
+            'name',
+            'recipe_count',
+            'quant_data', 
+            'nut_info'
+            )
 
 class CuisinesSerializer(serializers.ModelSerializer) :
     class Meta:
         model = Cuisines
-        field = (id,cuisine,name,url,recipe_count,quant_data)
+        fields = (
+            'id',
+            'cuisine',
+            'name,url',
+            'recipe_count',
+            'quant_data'
+            )
 
 
  
