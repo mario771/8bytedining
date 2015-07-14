8587b7f7821e61d233c6b50e8049afa0514a462a
Marguerite West-Driga <mwestdri@gmail.com>
2015-07-13 22:31:55 -0500

    setting admin.py back to original content types

--- food/foodApp/admin.py
+++ food/foodApp/admin.py
@@ -1,14 +1,12 @@
 from django.contrib import admin
 
-#from .models import Recipes
-#from .models import Ingredients
-#from .models import Cuisines
-from .models import Thing
+from .models import Recipes
+from .models import Ingredients
+from .models import Cuisines
 
-#admin.site.register(Recipes)
-#admin.site.register(Ingredients)
-#admin.site.register(Cuisine)
-admin.site.register(Thing)
+admin.site.register(Recipes)
+admin.site.register(Ingredients)
+admin.site.register(Cuisines)
 
 
 
