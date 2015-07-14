ceda804e49a566f31acc8fb719eb8a2fc97d3bea
Marguerite West-Driga <mwestdri@gmail.com>
2015-07-13 21:10:58 -0500

    updated model design with ManyToManyField

--- food/foodApp/models.py
+++ food/foodApp/models.py
@@ -2,76 +2,73 @@ from django.db import models
 
 # Create your models here.
 
+#------------------
+#Ingredients Model
+#------------------
+
+class Ingredients (models.Model) :
+   ing_id = models.CharField(max_length=500, default="") 
+   name = models.CharField(max_length=500, default="")
+   recipe_count = models.IntegerField(default=0)
+   quant_data = models.CharField(max_length=500, default="")
+   nut_info = models.CharField(max_length=5000)
+  
+   def get_absolute_url(self):
+        url_name = self.full_name.replace(' ', '_')
+        return "/ingredients/%s/" % url_name
+
+   def __str__ (self):
+        return self.full_name
+#------------
+#Cuisines Model
+#------------
+
+class Cuisines(models.Model) :
+
+   """
+   Cuisines contains a name, an id (name), url, and recipe count
+   """
+   cuisine = models.CharField(max_length=500)
+   name = models.CharField(max_length=500)
+   url = models.CharField(max_length=500)
+   recipe_count = models.IntegerField(default=0)
+   quant_data = models.CharField(max_length=5000)
+
+   def get_absolute_url(self):
+        url_name = self.full_name.replace(' ', '_')
+        return "/cuisine/%s/" % url_name
+
+   def __str__ (self):
+        return self.full_name
 #-------------
 #Recipe Model
 #-------------
 
-#class Recipes (models.Model) : 
-#
+class Recipes (models.Model) : 
+
 # """ 
 #  The Recipe model returns name, id (recipe name)  url,
 #  cuisine, cooking method, ingredients, and image
 # """
 
-# name = models.CharField(max_length=500)
-# recipe_id = models.CharField(max_length=500)
-# url = models.CharField(max_length=500)
-# directions = models.CharField(max_length= 5000, default = "")
-# nutritionalInfo = models.CharField(max_length=5000, default = "")
-# cuisineGivenRecipe = models.CharField(max_length=500)
-# ingredients = models.ForeignKey("Ingredients", default = "")
-# # How to put ingredients into list?? I think there's models.ManytoMany ?
-# image = models.CharField(max_length=500)
-#
-# def get_absolute_url(self):
-#        url_name = self.full_name.replace(' ', '_')
-#        return "/Recipes/%s/" % url_name
-#
-# def __str__ (self):
-#        return self.full_name
-#  
-##------------------
-##Ingredients Model
-##------------------
-#
-#class Ingredients (models.Model) :
-#
-#   name = models.CharField(max_length=500, default="")
-#   recipe_count = models.IntegerField(default=0)
-#
-#   
-#  
-#   def get_absolute_url(self):
-#        url_name = self.full_name.replace(' ', '_')
-#        return "/ingredients/%s/" % url_name
-#
-#   def __str__ (self):
-#        return self.full_name
-#
-##------------
-##Cuisine Model
-##------------
-#
-#class Cuisine(models.Model) :
-#
-#   """
-#   Cuisine contains a name, an id (name), url, and recipe count
-#   """
-#
-#   name = models.CharField(max_length=500)
-#   cuisine_id = models.CharField(max_length=500)
-#   url = models.CharField(max_length=500)
-#   recipe_count = models.IntegerField(default=0)
-#   ingredients = models.ForeignKey('Ingredients', default = "")
-#   recipes = models.ForeignKey("Recipes", default = "")
-#   def get_absolute_url(self):
-#        url_name = self.full_name.replace(' ', '_')
-#        return "/cuisine/%s/" % url_name
-#
-#   def __str__ (self):
-#        return self.full_name
+   name = models.CharField(max_length=500)
+   recipe_id = models.CharField(max_length=500)
+   directions = models.CharField(max_length= 5000, default = "")
+   cuisine = models.ForeignKey(Cuisines)
+   ingredients = models.ManyToManyField("Ingredients", default = "")
+   # How to put ingredients into list?? I think there's models.ManytoMany ?
+   img = models.CharField(max_length=500)
+   nut_info = models.CharField(max_length=5000)
+   quant_data = models.CharField(max_length=500)
 
-class Thing(models.Model) :
-   name = models.CharField(max_length=20)
-   number = models.IntegerField(default=0)
+
+   def get_absolute_url(self):
+        url_name = self.full_name.replace(' ', '_')
+        return "/Recipes/%s/" % url_name
+
+   def __str__ (self):
+        return self.full_name
+  
+
+
 
