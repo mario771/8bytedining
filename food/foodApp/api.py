aa94dd3470e36d457848cf9d49d6a0d1d03efcdf
Peter Wong <peterpaulwong@Peter-Wongs-MacBook-Pro.local>
2015-07-14 23:24:33 -0500

    adding more the api models

--- food/foodApp/api.py
+++ food/foodApp/api.py
@@ -26,5 +26,45 @@ class RecipeDetail(APIView):
       serialized_recipe = RecipesSerializer(recipe)
       return Response(serialized_recipe.data)
 
+class CuisineList(APIView):
+
+   def get(self,request,format=None):
+      cuisines = Cuisines.objects.filter()
+      serialized_cuisines = CuisinesSerializer(cuisines,many=True)
+      return Response(serialized_cuisines.data)
+
+class CuisineDetail(APIView):
+
+   def get_object(self,pk):
+      try:
+         return Cuisines.objects.get(pk=pk)
+      except Cuisines.DoesNotExist :
+         raise Http404
+
+   def get(self,request,pk,format=None):
+      cuisine = self.get_object(pk)
+      serialized_cuisine = CuisinesSerializer(cuisine)
+      return Response(serialized_cuisine.data)
+
+
+class IngredientList(APIView):
+
+   def get(self,request,format=None):
+      ingredients = Ingredients.objects.filter()
+      serialized_ingredients = IngredientsSerializer(ingredients,many=True)
+      return Response(serialized_ingredients.data)
+
+class IngredientDetail(APIView):
+
+   def get_object(self,pk):
+      try:
+         return Ingredients.objects.get(pk=pk)
+      except Ingredients.DoesNotExist :
+         raise Http404
+
+   def get(self,request,pk,format=None):
+      ingredient = self.get_object(pk)
+      serialized_ingredient = IngredientsSerializer(ingredient)
+      return Response(serialized_ingredient.data)
 
 
