c17f8b886736c829f476e0c49352eeff8ad7a1cf
Peter Wong <peterpaulwong@wireless-10-147-38-110.public.utexas.edu>
2015-07-21 11:47:55 -0500

    adding crossfit 4

--- food/foodApp/views.py
+++ food/foodApp/views.py
@@ -135,7 +135,7 @@ def crossfit(request):
   teamApiUrl = "http://crossfit.social/"
   requestRegions = urllib.request.urlopen(teamApiUrl+"api/regions/?format=json")
   requestAthletes = urllib.request.urlopen(teamApiUrl+"api/athletes/?format=json")
-  requestInstagrams = urlib.request.urlopen(teamApiUrl+"api/instagram/?format=json")
+  requestInstagrams = urllib.request.urlopen(teamApiUrl+"api/instagram/?format=json")
 
   responseRegions = requestDivisions.read().decode("utf-8")
   regionList = json.loads(responseDivisions)
