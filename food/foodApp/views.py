bf2d1b5316107150e79b0e2029571ca87e69b3f6
Peter Wong <peterpaulwong@wireless-10-147-38-110.public.utexas.edu>
2015-07-21 18:04:56 -0500

    adding unittest about to file 15

--- food/foodApp/views.py
+++ food/foodApp/views.py
@@ -209,5 +209,9 @@ def runTests(request):
 	command = "python3 " + os.path.join(BASE_DIR, 'manage.py') + " test foodApp  --keepdb"
 	pipe = subprocess.Popen(command.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 	result = pipe.stdout.readlines() + pipe.stderr.readlines()
+        print("****************************************************")
+        print(result)  
+        print("****************************************************")
+
 	return render_to_response('recipes.html', {'result': result})
 
