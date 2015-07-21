b3b6a6c657bf261e92de94e521a64b53a7755663
Marguerite West-Driga <mwestdri@gmail.com>
2015-07-21 18:13:50 -0500

    Jesus fixed search, or started to, anyway

--- food/foodApp/search.py
+++ food/foodApp/search.py
@@ -1,7 +1,5 @@
 import re
 
-##Search impl from Julien Phalip 
-
 from django.db.models import Q
 
 def normalize_query(query_string,
@@ -9,28 +7,24 @@ def normalize_query(query_string,
                     normspace=re.compile(r'\s{2,}').sub):
     ''' Splits the query string in invidual keywords, getting rid of unecessary spaces
         and grouping quoted words together.
-
+        Example:
+        
+        >>> normalize_query('  some random  words "with   quotes  " and   spaces')
+        ['some', 'random', 'words', 'with quotes', 'and', 'spaces']
+    
     '''
     return [normspace(' ', (t[0] or t[1]).strip()) for t in findterms(query_string)] 
 
-def dprint(a):
-    print("======================")
-    print(str(a))
-    print("======================")
-
-def get_query(both, query_string, search_fields):
+def get_query(query_string, search_fields):
     ''' Returns a query, that is a combination of Q objects. That combination
         aims to search keywords within a model by testing the given search fields.
     
     '''
     query = None # Query to search for every search term        
-    right_fields = []
     terms = normalize_query(query_string)
     for term in terms:
         or_query = None # Query to search for a given term in each field
         for field_name in search_fields:
-            if term in field_name: 
-                right_fields += field_name
             q = Q(**{"%s__icontains" % field_name: term})
             if or_query is None:
                 or_query = q
@@ -39,10 +33,5 @@ def get_query(both, query_string, search_fields):
         if query is None:
             query = or_query
         else:
-            if both == 1 :
-                query = query & or_query
-            if both == 0 :
-                query = query | or_query
-            else :
-                query = query
+            query = query & or_query
     return query
